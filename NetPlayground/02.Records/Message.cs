f3c216cec62aebfbbb04b59c04e2f3d40242a646
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2021-01-02 00:38:30 +0200

    Add example of record types in c# 9.

---
+++ NetPlayground/02.Records/Message.cs
@@ -0,0 +1,27 @@
+﻿namespace Records
+{
+    using System;
+    
+    internal record Message
+    {
+        public Message()
+        {
+        }
+
+        public Message(string content, string author, DateTime creationTime, DateTime? lastModifiedTime = null)
+        {
+            this.Content = content;
+            this.Author = author;
+            this.CreationTime = creationTime;
+            this.LastModifiedTime = lastModifiedTime;
+        }
+
+        public string Content { get; set; }
+
+        public string Author { get; set; }
+
+        public DateTime CreationTime { get; set; }
+
+        public DateTime? LastModifiedTime { get; set; }
+    }
+}
