3ccdfd52eaceba5440eeed22db08183d8a162c4b
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2021-01-02 19:28:57 +0200

    Fix message typo.

--- 07.Relational and Logical patterns/Startup.cs
+++ 07.Relational and Logical patterns/Startup.cs
@@ -14,7 +14,7 @@
                 <= 0 => "Less than or equal to 0",
                 > 0 and <= 10 => "More than 0 but less than or equal to 10",
                 > 10 and not 666 and not 1313 => "More than 10, but not equal to 666 or 1313",
-                666 or 1313 => "Symbolic numbers"
+                666 or 1313 => "Symbolic number"
             };
 
             Console.WriteLine(message);
